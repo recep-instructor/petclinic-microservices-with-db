afb49289bc4f886235f694193752a07c7c0b1b76
recep-instructor <125568888+recep-instructor@users.noreply.github.com>
2023-06-14 21:33:16 +0300

    Update main.tf

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -147,7 +147,7 @@ EOF
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
 }
 
-resource "aws_iam_instance_profile" "petclinic-master-server-profile-recep" {
+resource "aws_iam_instance_profile" "petclinic-master-server-profile" {
   name = "petclinic-master-server-profile-recep"
   role = aws_iam_role.petclinic-master-server-s3-role.name
 }
